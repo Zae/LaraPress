51b1796a786bbaf27516f31384537102872d1b8d
Ezra Pool <ezra@digitalnatives.nl>
2016-01-06 15:37:57 +0100

    temporary disable the interfaces because they work differently for L4 and L5

--- src/models/User.php
+++ src/models/User.php
@@ -2,10 +2,7 @@
 
 namespace Zae\LaraPress;
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
-class User extends \Eloquent implements UserInterface, RemindableInterface {
+class User extends \Eloquent {
 
 	protected $primaryKey = 'ID';
 	protected $appends = ['id', 'login', 'nicename', 'email', 'URL', 'registered', 'activation_key', 'status'];
@@ -91,35 +88,4 @@ class User extends \Eloquent implements UserInterface, RemindableInterface {
 	public function getStatusAttribute() {
 		return $this->attributes['user_status'];
 	}
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->user_pass;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->user_email;
-	}
-
 }
